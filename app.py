3939b06a4e9fb0f6ce082916f17b2c61a7266e88
Oliver Kielian <oliverkielian02@gmail.com>
2025-04-27 16:28:38 -0600

    trying to get bevy game embedded
    erros

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+import os
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = 'mycoolawesomekey'
+CORS(app)
 
 @app.route('/', methods=["GET"])
 def home():
@@ -14,6 +17,20 @@ def art():
 @app.route('/game', methods=["GET"])
 def game():
      return render_template("game.html")
+
+@app.route('/game/claw_machine', methods=["GET"])
+def claw_machine():
+    return render_template("index.html")
+
+@app.route('/static/game/<path:filename>')
+def serve_game_assets(filename):
+    if filename.endswith('.wasm'):
+        return send_from_directory(os.path.join(app.root_path, 'static', 'game'), filename, mimetype='application/wasm')
+    return send_from_directory(os.path.join(app.root_path, 'static', 'game'), filename)
+
+@app.route('/game/assets/<path:filename>')
+def serve_assets(filename):
+    return send_from_directory(os.path.join(app.root_path, 'game', 'assets'), filename)
 
 @app.route('/contact', methods={"GET", "POST"})
 def contact():
